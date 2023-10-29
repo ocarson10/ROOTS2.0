f6b50e686d60b04c542fcf72b7a6ace65188402b
Ian McKendrick <ibmckend@ncsu.edu>
2023-10-29 19:11:12 -0400

    QOL Change - Avoiding default to invalid unless loading is complete.

--- client/src/App.js
+++ client/src/App.js
@@ -19,43 +19,53 @@ LicenseInfo.setLicenseKey(
 
 function App() {
   const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    try {
-      instance.get("/users/current").then((response) => {
-        setUser(response.data);
-      });
-    } catch (error) {
-      console.log('Error finding current user.\n' + error);
+    const loadCurrentUser = async () => {
+      try {
+        await instance.get("/users/current").then((response) => {
+          setUser(response.data);
+        });
+      } catch (error) {
+        console.log('Error finding current user.\n' + error);
+      }
+      setIsLoading(false);
     }
+    
+    loadCurrentUser();
   }, []);
 
   console.log("user: ", user);
   return (
-    <Router>
-      {user ? (
-        <>
-          <Header user={user} />
-          <Routes>
-            <Route path="/" element={<Home user={user} />} />
-            <Route path="/material/:material/:action/:id?" element={<MaterialForm />} />
-            <Route path="/report/:type/:id/" element={<Report />} />
-            <Route path="/qr/read" element={<QRCodeReader />} />
-            <Route path="/user-management" element={<UserManagement user={user} />} />
-            <Route path="/logs" element={<Logs user={user} />} />
-            <Route path="/invalid" element={<Invalid />} />
-            <Route path="*" element={<Invalid />} />
-          </Routes>
-        </>
-      ) : (
-        <>
-          <Header />
-          <Routes>
-            <Route path="*" exact={true} element={<Invalid />} />
-          </Routes>
-        </>
-      )}
-    </Router>
+    <>
+      {!isLoading && 
+        <Router>
+          {user ? (
+            <>
+              <Header user={user} />
+              <Routes>
+                <Route path="/" element={<Home user={user} />} />
+                <Route path="/material/:material/:action/:id?" element={<MaterialForm />} />
+                <Route path="/report/:type/:id/" element={<Report />} />
+                <Route path="/qr/read" element={<QRCodeReader />} />
+                <Route path="/user-management" element={<UserManagement user={user} />} />
+                <Route path="/logs" element={<Logs user={user} />} />
+                <Route path="/invalid" element={<Invalid />} />
+                <Route path="*" element={<Invalid />} />
+              </Routes>
+            </>
+          ) : (
+            <>
+              <Header />
+              <Routes>
+                <Route path="*" exact={true} element={<Invalid />} />
+              </Routes>
+            </>
+          )}
+        </Router>
+      }
+    </>
   );
 }
 
